48e4a032ca69d63b41e86d7f19a54e4a8f6615bd
levimigom <migomlevi@hotmail.com>
2025-12-17 12:03:26 +0100

    2025 day 10 part 2

--- 2025/10_factory/part2_attempt1.c
+++ 2025/10_factory/part2_attempt1.c
@@ -51,75 +51,71 @@ struct Machine **read_input(int *machine_count){
 	return machines;
 }
 
-int fewest_button_presses(struct Machine *machine, int button, int *joltage_levels, int total_joltage){
+int dfs_fewest_button_presses(struct Machine *machine, int button, int *joltage_levels, int total_joltage){
+	// dfs because the buttons are sorted in descending order of number of lights affected
+	// the first solution we find will be the one with the least button presses
+	// 	-> not sure about this, it is likely to be the minimum but not guaranteed i think	
+
+	// TODO: dfs is a dead end, try something new
+	// Ideas:	 
+	// 	- equations?
+	//		Input: (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+	//		Equation: (3A+5B+4C+7D) = a(D) + b(B+D) + c(B) + d(C+D) + e(A+C) + f(A+B) 
+	//		Solve for a,b,c,d,e and f => a = 1, b = 5, d = 1, e = 3 => 10
+	//		Looks like it could work, but how do i program this?
+
 	// solution found -> no more button presses needed
 	if(total_joltage == 0){
 		return 0;
 	}
-
-	int i, j, solution;
-
-	solution = -100000;
-
-	// if the remaining joltages are all even, divide by two and recurse
-	bool all_even = total_joltage % 2 == 0;
-	for(i = 0 ; all_even && i < machine->joltage_level_count; i++){
-		all_even = joltage_levels[i] % 2 == 0;
-	}
-
-	if(all_even){		
-		for(i = 0 ; i < machine->joltage_level_count; i++){
-			joltage_levels[i] /= 2;
-		}
-
-		int k = 2 * fewest_button_presses(machine, 0, joltage_levels, total_joltage / 2);
-		
-		for(i = 0 ; i < machine->joltage_level_count; i++){
-			joltage_levels[i] *= 2;
-		}
-
-		solution = k;
-	}
-
-	// all buttons passed
+	
+	// all buttons passed -> solution not found
 	if(button == machine->button_count){
-		return solution;
+		return -100000;
 	}
 
-	// do not press the current button
-	int no_button_press = fewest_button_presses(machine, button + 1, joltage_levels, total_joltage);
+	int i, j, solution, max_presses;
 
 	// find the maximum number of times the current button can be pushed
-	int max_presses = joltage_levels[machine->buttons[button]->lights[0]];
+	max_presses = joltage_levels[machine->buttons[button]->lights[0]];
 	for(i = 1; i < machine->buttons[button]->light_count; i++){
 		max_presses = min(max_presses, joltage_levels[machine->buttons[button]->lights[i]]);
 	}
 
-	// try to press the button once
-	int one_button_press = -100000;
-	
-	if(0 < max_presses){
-		for(i = 0; i < machine->buttons[button]->light_count; i++){
-			joltage_levels[machine->buttons[button]->lights[i]]--;
-		}
+	// dfs starting from the maximum number of button presses
+	total_joltage -= max_presses * machine->buttons[button]->light_count;
+	for(i = 0; i < machine->buttons[button]->light_count; i++){
+		joltage_levels[machine->buttons[button]->lights[i]] -= max_presses;
+	}
+
+	for(i = max_presses; 0 < i; i--){
+		// dfs having pressed this button i times
+		solution = dfs_fewest_button_presses(machine, button + 1, joltage_levels, total_joltage);
 		
-		one_button_press = 1 + fewest_button_presses(machine, button + 1, joltage_levels, total_joltage - machine->buttons[button]->light_count);
+		// check if solution has been found
+		if(0 <= solution){
+			return solution + i;
+		}
 
-		for(i = 0; i < machine->buttons[button]->light_count; i++){
-			joltage_levels[machine->buttons[button]->lights[i]]++;
+		// solution not found -> adjust joltage levels for next iteration
+		total_joltage += machine->buttons[button]->light_count;
+		for(j = 0; j < machine->buttons[button]->light_count; j++){
+			joltage_levels[machine->buttons[button]->lights[j]]++;
 		}
 	}
 
-	// return minimal solution (if found)
-	if(0 <= no_button_press && (solution < 0 || no_button_press < solution)){
-		solution = no_button_press;
-	}
+	// dfs without having pressed the button
+	return dfs_fewest_button_presses(machine, button + 1, joltage_levels, total_joltage);
+}
 
-	if(0 <= one_button_press && (solution < 0 || one_button_press < solution)){
-		solution = one_button_press;
-	}
-
-	return solution;
+int compare( const void* a, const void* b)
+{
+	struct Button *button_a = *((struct Button**) a);
+     	struct Button *button_b = *((struct Button**) b);
+     
+     	if(button_a->light_count == button_b->light_count) return 0;
+     	else if(button_a->light_count < button_b->light_count) return 1;
+     	else return -1;
 }
 
 int preprocess(struct Machine *machine, int *joltage_levels, int *total_joltage){
@@ -143,7 +139,7 @@ int preprocess(struct Machine *machine, int *joltage_levels, int *total_joltage)
 
 		if(1 < button_count) continue;
 		
-		//printf("Joltage %d is only affected by one button (%d)!\n", i, button_id);
+		printf("Joltage %d is only affected by one button (%d)!\n", i, button_id);
 
 		k = joltage_levels[i];
 		for(j = 0; j < machine->buttons[button_id]->light_count; j++){
@@ -162,10 +158,15 @@ void solve(){
 	int i, j, button_presses, total_button_presses, machine_count;
 	struct Machine **machines = read_input(&machine_count);
 
+	// order buttons by count
+	for(i = 0; i < machine_count; i++){
+		qsort(machines[i]->buttons, machines[i]->button_count, sizeof(struct Button*), compare);
+	}
+
 	// calculate solution
 	total_button_presses = 0;
 	for(i = 0; i < machine_count; i++){
-		printf("Machine %d: ", i);
+		printf("Machine %d:\n", i);
 		//machine_print(machines[i]);
 
 		int *joltage_levels, total_joltage;
@@ -177,8 +178,8 @@ void solve(){
 		}
 
 		button_presses = preprocess(machines[i], joltage_levels, &total_joltage);
-		button_presses += fewest_button_presses(machines[i], 0, joltage_levels, total_joltage);
-		printf("%d\n", button_presses);
+		button_presses += dfs_fewest_button_presses(machines[i], 0, joltage_levels, total_joltage);
+		printf("\tButton presses %d: %d\n", i, button_presses);
 
 		total_button_presses += button_presses;
 		free(joltage_levels);
--- 2025/10_factory/part2_attempt2.c
+++ 2025/10_factory/part2_attempt2.c
@@ -51,16 +51,23 @@ struct Machine **read_input(int *machine_count){
 	return machines;
 }
 
-int fewest_button_presses(struct Machine *machine, int button, int *joltage_levels, int total_joltage){
+int dfs_fewest_button_presses(struct Machine *machine, int button, int *joltage_levels, int total_joltage){
+	// dfs because the buttons are sorted in descending order of number of lights affected
+	// the first solution we find will be the one with the least button presses
+	// 	-> not sure about this, it is likely to be the minimum but not guaranteed i think	
+
 	// solution found -> no more button presses needed
 	if(total_joltage == 0){
 		return 0;
 	}
+	
+	// all buttons passed -> solution not found
+	if(button == machine->button_count){
+		return -100000;
+	}
 
-	int i, j, solution;
-
-	solution = -100000;
-
+	int i, j, solution, max_presses;
+	
 	// if the remaining joltages are all even, divide by two and recurse
 	bool all_even = total_joltage % 2 == 0;
 	for(i = 0 ; all_even && i < machine->joltage_level_count; i++){
@@ -72,54 +79,50 @@ int fewest_button_presses(struct Machine *machine, int button, int *joltage_leve
 			joltage_levels[i] /= 2;
 		}
 
-		int k = 2 * fewest_button_presses(machine, 0, joltage_levels, total_joltage / 2);
-		
-		for(i = 0 ; i < machine->joltage_level_count; i++){
-			joltage_levels[i] *= 2;
-		}
-
-		solution = k;
+		return 2 * dfs_fewest_button_presses(machine, button, joltage_levels, total_joltage / 2);
 	}
 
-	// all buttons passed
-	if(button == machine->button_count){
-		return solution;
-	}
-
-	// do not press the current button
-	int no_button_press = fewest_button_presses(machine, button + 1, joltage_levels, total_joltage);
 
 	// find the maximum number of times the current button can be pushed
-	int max_presses = joltage_levels[machine->buttons[button]->lights[0]];
+	max_presses = joltage_levels[machine->buttons[button]->lights[0]];
 	for(i = 1; i < machine->buttons[button]->light_count; i++){
 		max_presses = min(max_presses, joltage_levels[machine->buttons[button]->lights[i]]);
 	}
 
-	// try to press the button once
-	int one_button_press = -100000;
-	
-	if(0 < max_presses){
-		for(i = 0; i < machine->buttons[button]->light_count; i++){
-			joltage_levels[machine->buttons[button]->lights[i]]--;
-		}
+	// dfs starting from the maximum number of button presses
+	total_joltage -= max_presses * machine->buttons[button]->light_count;
+	for(i = 0; i < machine->buttons[button]->light_count; i++){
+		joltage_levels[machine->buttons[button]->lights[i]] -= max_presses;
+	}
+
+	for(i = max_presses; 0 < i; i--){
+		// dfs having pressed this button i times
+		solution = dfs_fewest_button_presses(machine, button + 1, joltage_levels, total_joltage);
 		
-		one_button_press = 1 + fewest_button_presses(machine, button + 1, joltage_levels, total_joltage - machine->buttons[button]->light_count);
+		// check if solution has been found
+		if(0 <= solution){
+			return solution + i;
+		}
 
-		for(i = 0; i < machine->buttons[button]->light_count; i++){
-			joltage_levels[machine->buttons[button]->lights[i]]++;
+		// solution not found -> adjust joltage levels for next iteration
+		total_joltage += machine->buttons[button]->light_count;
+		for(j = 0; j < machine->buttons[button]->light_count; j++){
+			joltage_levels[machine->buttons[button]->lights[j]]++;
 		}
 	}
 
-	// return minimal solution (if found)
-	if(0 <= no_button_press && (solution < 0 || no_button_press < solution)){
-		solution = no_button_press;
-	}
+	// dfs without having pressed the button
+	return dfs_fewest_button_presses(machine, button + 1, joltage_levels, total_joltage);
+}
 
-	if(0 <= one_button_press && (solution < 0 || one_button_press < solution)){
-		solution = one_button_press;
-	}
-
-	return solution;
+int compare( const void* a, const void* b)
+{
+	struct Button *button_a = *((struct Button**) a);
+     	struct Button *button_b = *((struct Button**) b);
+     
+     	if(button_a->light_count == button_b->light_count) return 0;
+     	else if(button_a->light_count < button_b->light_count) return 1;
+     	else return -1;
 }
 
 int preprocess(struct Machine *machine, int *joltage_levels, int *total_joltage){
@@ -143,7 +146,7 @@ int preprocess(struct Machine *machine, int *joltage_levels, int *total_joltage)
 
 		if(1 < button_count) continue;
 		
-		//printf("Joltage %d is only affected by one button (%d)!\n", i, button_id);
+		printf("Joltage %d is only affected by one button (%d)!\n", i, button_id);
 
 		k = joltage_levels[i];
 		for(j = 0; j < machine->buttons[button_id]->light_count; j++){
@@ -162,10 +165,15 @@ void solve(){
 	int i, j, button_presses, total_button_presses, machine_count;
 	struct Machine **machines = read_input(&machine_count);
 
+	// order buttons by count
+	for(i = 0; i < machine_count; i++){
+		qsort(machines[i]->buttons, machines[i]->button_count, sizeof(struct Button*), compare);
+	}
+
 	// calculate solution
 	total_button_presses = 0;
 	for(i = 0; i < machine_count; i++){
-		printf("Machine %d: ", i);
+		printf("Machine %d:\n", i);
 		//machine_print(machines[i]);
 
 		int *joltage_levels, total_joltage;
@@ -177,8 +185,8 @@ void solve(){
 		}
 
 		button_presses = preprocess(machines[i], joltage_levels, &total_joltage);
-		button_presses += fewest_button_presses(machines[i], 0, joltage_levels, total_joltage);
-		printf("%d\n", button_presses);
+		button_presses += dfs_fewest_button_presses(machines[i], 0, joltage_levels, total_joltage);
+		printf("\tButton presses %d: %d\n", i, button_presses);
 
 		total_button_presses += button_presses;
 		free(joltage_levels);
